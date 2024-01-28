a6f39e0d1c5fa18c237d0c6faa8446e999d06012
ShuvoGenex <shuvo@mevrik.com>
2024-01-28 22:30:53 +0600

    empty space added

--- component/atom/Input.tsx
+++ component/atom/Input.tsx
@@ -16,11 +16,12 @@ export const Input = ({label, type, placeholder, name, onChange, error, value}: 
                 {label}
             </label><br/>
             <input value={value} style={{padding: '5px 10px', width: '100%', marginTop: '3px'}} onChange={onChange} name={name} type={type} className=" shadow appearance-none border rounded w-full py-2 px-2 text-gray-700 leading-tight focus:outline-none focus:shadow-outline"   placeholder={placeholder} />
-            {error ? (
+            { error ? (
                 <span style={{color: 'red'}}>
                     This field must not be empty!
                 </span>
-                ) : null}
+                ) : null
+            }
         </div>
     )
 }
